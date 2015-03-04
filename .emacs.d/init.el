ee3f32e485a5cba4af8ad8ae5a5e7cb28c3ff7d8
Seth <seth@knotis.com>
2015-03-03 16:01:40 -0800

    Added My Current Emacs Dot Files
    <EOM>

---
+++ .emacs.d/init.el
@@ -0,0 +1,10 @@
+;; (add-to-list 'load-path "~/.emacs.d/cl-lib/")
+;; (require 'cl-lib)
+
+;; android-mode
+;; (add-to-list 'load-path "~/opt/android-mode")
+;; (require 'android-mode)
+;; (defcustom android-mode-sdk-dir "~/opt/android-sdk-linux")
+
+(setq tab-width 4)        ;; set your desired tab width
+(setq indent-tabs-mode t) ;; use tabs for indentation
